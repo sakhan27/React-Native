d1b806f247c6a2892e7a66520056eff50c833f4d
Sohaib Khan <sohaibkhan922@gmail.com>
2022-01-11 09:29:27 -0600

    Swipe and Delete Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -171,3 +171,7 @@ export const addComment = comment => ({
     payload: comment
 });
 
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+}); 
